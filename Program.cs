f85a05c2660093ea4c87ad74f55b93a1da9cde1b
MrDoctor225 <56573315+MrDoctor225@users.noreply.github.com>
2019-11-07 18:47:52 +0700

    u08uj

--- Program.cs
+++ Program.cs
@@ -4,268 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Файл
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*        1 int[,] file = new int[10, 10];
-              int sum = 0;
-              int[] mass = new int[10];
-              Random ran = new Random();
-              for (int i = 0; i < 10; i++)
-              {
-                  for (int j = 0; j < 10; j++)
-                  {
-                      file[i, j] = ran.Next(1,12);
-                      Console.Write("{0}\t", file[i,j]);
-                  }
-              }
-              for (int i = 0; i < 10; i++)
-              {
-                  sum =0;
-                  for (int j = 0; j < 10; j++)
-                  {
-                      sum = sum + file[i, j];
-
-                  }
-                  mass[i] = sum;
-                  Console.WriteLine(mass[i]);
-
-              }
-             int maxmass = 0;
-             for (int i = 0; i < 10; i++)
-              {
-                 if(mass[i]> maxmass) { maxmass = mass[i]; }
-
-              }
-              Console.WriteLine();
-              Console.WriteLine(maxmass);
-              Console.ReadKey();
-             
-                        2int[,] file = new int[4, 5];
-            Random ran = new Random();
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    file[i, j] = ran.Next(1, 9);
-                    Console.Write("{0}\t", file[i, j]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write("{0}\t", Math.Pow(file[i, j], 2));
-                }
-            Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write("{0}\t", (file[i, j]*(-1)));
-                }
-            Console.WriteLine();
-            }
-            Console.WriteLine();
-            Console.ReadKey();
-            
-                  3   int[,] scl = new int[7, 3];
-            int[] mass = new int[7];
-            int cnt = 0;
-            Random ran = new Random();
-            for (int i = 0; i < 7; i++)
-            {
-                mass[i] = 0;
-            }
-            for (int i = 0; i < 7; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    scl[i, j] = ran.Next(1, 6);
-                    Console.Write("{0}\t", scl[i, j]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 7; i++)
-            {
-                
-                for (int j = 0; j < 3; j++)
-                {
-                    if (scl[i,j] == 5)
-                    {
-                        mass[i]++;
-                    }                   
-                }
-                Console.Write("{0}\t", mass[i]);
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            cnt = 0;
-            for (int i = 0; i < 7; i++)
-            {
-                if (mass[i] == 0)
-                {
-                    cnt++;
-                }
-            }
-            Console.WriteLine(cnt);
-            Console.WriteLine();
-            Console.ReadKey();
-            
-            4  int[,] gr = new int[7, 4];
-            int[] success = new int[7];
-            double cnt = 0;
-            int avg = 0;
-            Random rnd = new Random();
-            for (int i = 0; i < 7; i++)
-            {
-                success[i] = 0;
-            }
-            for (int i = 0; i < 7; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    gr[i, j] = rnd.Next(2, 5);
-                    Console.Write("{0}\t", gr[i, j]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 7; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (gr[i, j] == 2)
-                    {
-                        success[i]++;
-                    } 
-                }
-                Console.Write("{0}\t", success[i]);
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 7; i++)
-            {
-                if (success[i] > 0)
-                {
-                    cnt++;
-                }
-            }
-            for (int i = 0; i < 7; i++)
-            {
-                for (int j =0; j < 4; j++)
-                {
-                    cnt = cnt + gr[i, j];
-                }
-            }
-            Console.WriteLine(cnt);
-            cnt = cnt / 28;
-            Console.WriteLine(cnt);
-            Console.WriteLine();
-            Console.ReadKey();
-            
-            5 int[,] mass = new int[4, 5];
-            int[] resp = new int[4];
-            int k = -1;
-            Random rnd = new Random();
-            for (int i = 0; i < 4; i++)
-            {
-                resp[i] = 0;
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    mass[i, j] = rnd.Next(1, 4);
-                    Console.Write("{0}\t", mass[i, j]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            for (int i = 0; i < 4; i++)
-            {
-                k = -1;
-                for (int j = 0; j < 4; j++)
-                {
-                    for (int g = j + 1; g < 3; g++)
-                    {
-                        if (mass[i, j] == mass[i, g])
-                        {
-                            k = i;
-                        }
-                    }
-                }
-                Console.WriteLine();
-                Console.WriteLine(k);
-                Console.ReadKey();
-            }
-            */
-            int[,] field = new int[3, 3];
-            bool z = true;
-            int cnt0 = 0;
-            int cnt1 = 0;
-            int k = -1;
             int x = 0;
             int y = 0;
-            int n = 0;
-            for (int i = 0; i < 3; i++)
+            bool z = true;
+            int[,] graph = new int [6, 6];
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < 5; j++)
                 {
-                    field[i, j] = -1;
-                    Console.Write("{0}\t", field[i, j]);
+                    graph[i, j] = rnd.Next(1,1);
+                    Console.Write("{0}\t", graph[i, j]);
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
             while (z)
-            {   n++;
-                Console.Write("Введите координату строки ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите координату ряда ");
-                y = Convert.ToInt32(Console.ReadLine());
-                field[x, y] =1;
-                Console.Write("Введите координату строки ");
-                x = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите координату ряда ");
-                y = Convert.ToInt32(Console.ReadLine());
-                field[x, y] = 0;
-
-               for (int l = 0; l < 3; l++)
-               {
-                    for(int p = 0; p < 3; p++)
-                    if (field [x,y] == 0)
-                    {
-                        cnt0++;
-                    }
-
-               }
+            {
                 
-               
-                for (int p = 0; p < 3; p++)
-                {
-                    for (int l = 0; l < 3; l++)
-                    {
-                        
-                        Console.Write("{0}\t", field[p, l]);
-                    }
-                    Console.WriteLine();
-                }
-                Console.WriteLine();
-                if (n == 2)
+                Console.Write("Введите координату строки!  ");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите координату ряда!  ");
+                y = Convert.ToInt32(Console.ReadLine());
+                graph[x, y] = 0;
+                if (x==9 && y==9)
                 {
                     z = false;
                 }
             }
             Console.WriteLine();
+            Console.Write("{0}\t", graph[x, y]);
             Console.ReadKey();
         }
     }
